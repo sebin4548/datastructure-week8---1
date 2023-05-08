56d56b32cd343d8133ab9764d8147d6daeffd57b
sebin <sebin4548@gmail.com>
2023-05-08 14:53:26 +0900

    Initial Commit

---
+++ datastructure week8/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  datastructure week8
+//
+//  Created by KWON SEBIN on 2023/05/08.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    cout<<"initial git repo";
+    cout<<"ahffkTlqkfshadk";
+}
